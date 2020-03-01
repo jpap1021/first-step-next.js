ea16458de3bcb5b2a3ad9b1745d8e37ba80a3e70
juan arteaga <juanarteaga@MacBook-Pro-de-juan.local>
2020-03-01 17:20:50 -0500

    summary and styles character

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -4,6 +4,23 @@ const Post = props => (
   <div>
     <h1>{props.value.name}</h1>
     {props.value.image ? <img src={props.value.image} /> : null}
+    <h3>Sumary</h3>
+    <ul>
+      <li>Status: {props.value.status} </li>
+      <li>Specie: {props.value.species} </li>
+      <li>Gender: {props.value.gender}</li>
+    </ul>
+    <style jsx>
+      {`
+        div {
+          text-align: center;
+        }
+        ul {
+          list-style-type: none;
+          padding: 0;
+        }
+      `}
+    </style>
   </div>
 );
 
